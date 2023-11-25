a9e86c7f54957f6c0a6fd6fc253bdb62e870eb22
Isabella Campos <isabellaszcamposs@gmail.com>
2023-11-25 09:04:38 -0300

    feat: create app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'active-details' },
+  { path: 'active-details', loadChildren: () => import('./active-details/active-details.module').then(m => m.ActiveDetailsModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
