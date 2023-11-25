b88cfe9fca5b87930e3a163dd74c546ce6b6591d
Isabella Campos <isabellaszcamposs@gmail.com>
2023-11-25 11:16:38 -0300

    feat: add getResult and herokuCors

--- src/app/active-details/service/active-details.service.ts
+++ src/app/active-details/service/active-details.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Result } from '../models/Result.interface';
+import { Chart } from '../models/Chart.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActiveDetailsService {
+  private readonly corsHeroku: string = 'https://cors-anywhere.herokuapp.com';
+  private readonly urlApi: string = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) { }
 
-  getChart(activeCode: string): Observable<any> {
-    return this.http.get<any>(`${environment.apiBaseUrl}/v8/finance/chart/${activeCode}`)
+  getChart(activeCode: string): Observable<Chart> {
+    return this.http.get<Chart>(`${this.corsHeroku}/${this.urlApi}/v8/finance/chart/${activeCode}`)
+  }
+
+  getResult(activeCode: string): Observable<Result[]> {
+    return this.getChart(activeCode)
+      .pipe(map(chart => chart.result));
   }
 }
