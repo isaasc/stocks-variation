11d9edaa919f0b62aed17b73a6ff17c36bdd388b
Isabella Campos <isabellaszcamposs@gmail.com>
2023-11-25 11:13:41 -0300

    chore: adjust routing and modules from app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'active-details' },
+  { path: '', redirectTo: 'active-details', pathMatch: 'full' },
   { path: 'active-details', loadChildren: () => import('./active-details/active-details.module').then(m => m.ActiveDetailsModule)}
 ];
 
