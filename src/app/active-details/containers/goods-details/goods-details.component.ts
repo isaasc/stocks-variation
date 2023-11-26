6a7443317eeccd13689f79cb5c792dfde903ec50
Isabella Campos <isabellaszcamposs@gmail.com>
2023-11-26 19:13:33 -0300

    feat: adjust good-details to pass result to children

--- src/app/active-details/containers/goods-details/goods-details.component.ts
+++ src/app/active-details/containers/goods-details/goods-details.component.ts
@@ -1,20 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { GoodsDetailsService } from '../../service/goods-details.service';
 import { Observable } from 'rxjs';
+import { Result } from '../../models/Result.interface';
 
 @Component({
   selector: 'app-goods-detailss',
   templateUrl: './goods-details.component.html',
   styleUrls: ['./goods-details.component.scss'],
-  // changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GoodsDetailsComponent implements OnInit {
-  test$!: Observable<any>;
+  chartResult$!: Observable<Result>;
+  timestamps!: string[];
 
-  constructor(private activeDetails: GoodsDetailsService) {}
+  constructor(private goodsDetails: GoodsDetailsService) {}
 
   ngOnInit(): void {
-   this.test$ = this.activeDetails.getChart('PETR4.SA');
+    const timestampAtual = Math.floor(Date.now() / 1000);
+    const timestamp30DiasAtras = Math.floor((Number(this.getTimestamps(new Date(), 30)))/1000);
+
+    this.chartResult$ =this.goodsDetails.getResult('PETR4.SA', timestamp30DiasAtras, timestampAtual, '1d');
+  }
+
+  getTimestamps(diaInicial: Date, diasUteis: number) {
+    const diaSemana = diaInicial.getDay();
+    if (diaSemana === 0) {
+      diaInicial.setDate(diaInicial.getDate() - 2);
+    } else if (diaSemana === 6) {
+      diaInicial.setDate(diaInicial.getDate() - 1);
+    }
+
+    const data = new Date(diaInicial.getTime());
+    let diasContados = 0;
+
+    while (diasContados < diasUteis) {
+      data.setDate(data.getDate() - 1);
+      if (data.getDay() !== 6 && data.getDay() !== 0) {
+        diasContados++;
+      }
+    }
+    return data;
   }
 
 }
+
+
