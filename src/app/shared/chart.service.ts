42e8a9e18efc0ba59249411ac10c808f9f032c4e
Isabella Campos <isabellaszcamposs@gmail.com>
2023-11-26 23:26:14 -0300

    feat: create chart service

---
+++ src/app/shared/chart.service.ts
@@ -0,0 +1,72 @@
+import { Injectable } from '@angular/core';
+import { ChartConfiguration, ChartTypeRegistry } from 'chart.js';
+import { Chart } from 'chart.js/auto';
+import { Result } from '../active-details/models/Result.interface';
+import { TimestampUtility } from '../core/utilities/timestamp.utility';
+import { NumberUtility } from '../core/utilities/number.utility';
+import { VariationService } from '../active-details/service/variation.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChartService {
+  constructor(private timestampUtility: TimestampUtility, private numberUtility: NumberUtility, private variationService: VariationService) { }
+
+  createChartPriceVariationWithDailyVariationAndVariationFromFirstDate(chart: Result): ChartConfiguration<keyof ChartTypeRegistry> {
+    return {
+      type: 'line',
+      data: {
+        labels: this.timestampUtility.formatTimestampsForDisplay(chart.timestamp),
+        datasets: [{
+          label: 'Open',
+          data: chart.indicators.quote[0].open.map((open: string) => Number(this.numberUtility.formatNumber(Number(open), 2))),
+          borderColor:'#133A3B',
+          backgroundColor:'#133A3B80',
+          fill: false,
+          borderWidth: 2,
+          order: 0
+        },
+        {
+          label: 'Variação D-1',
+          data: this.variationService.calculateDailyVariation(chart.indicators),
+          borderColor: '#f2c110',
+          backgroundColor: '#f2c11080',
+          fill: false,
+          order: 1,
+          borderDash: [5, 5],
+          type: 'line',
+        },
+        {
+          label: 'Variação primeira data',
+          data: this.variationService.calculateVariationFromFirstDate(chart.indicators),
+          order: 2,
+          borderColor: '#5a189a',
+          backgroundColor: '#5a189a80',
+          fill: false,
+          borderDash: [5, 5],
+          type: 'line',
+        }]
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          title: {
+            display: true,
+            text: 'Variação de preço'
+          }
+        },
+        scales: {
+          y: {
+            ticks: {
+              stepSize: 8
+            }
+          }
+        }
+      },
+    };
+  }
+
+  renderChart(element: any, chartConfig: any): Chart {
+    return new Chart(element, chartConfig);
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
+import { ChartService } from './chart.service';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers: [
+    ChartService
   ]
 })
 export class SharedModule { }
