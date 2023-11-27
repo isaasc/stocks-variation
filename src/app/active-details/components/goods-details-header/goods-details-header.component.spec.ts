0f66fd5e06af5acb5f3dca8220972b5f2d68ad2a
Isabella Campos <isabellaszcamposs@gmail.com>
2023-11-27 03:25:30 -0300

    test: create goods-details-header test

--- src/app/active-details/components/goods-details-header/goods-details-header.component.spec.ts
+++ src/app/active-details/components/goods-details-header/goods-details-header.component.spec.ts
@@ -1,21 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { GoodsDetailsHeaderComponent } from './goods-details-header.component';
+import { Meta } from '../../models/Meta.interface';
+import { metaMock } from '../../models/mocks/ChartResult.mocks';
 
 describe('GoodsDetailsHeaderComponent', () => {
   let component: GoodsDetailsHeaderComponent;
   let fixture: ComponentFixture<GoodsDetailsHeaderComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [GoodsDetailsHeaderComponent],
+    }).compileComponents();
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [GoodsDetailsHeaderComponent]
-    });
     fixture = TestBed.createComponent(GoodsDetailsHeaderComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the correct title and title--principal', () => {
+    const meta: Meta = metaMock;
+    component.goods = meta;
+
+    fixture.detectChanges();
+
+    const titleElement: HTMLElement = fixture.nativeElement.querySelector('.title');
+    const titlePrincipalElement: HTMLElement = fixture.nativeElement.querySelector('.title--principal');
+
+    expect(titleElement.textContent).toContain('Detalhes do Ativo');
+    expect(titlePrincipalElement.textContent).toContain('AAPL');
   });
 });
