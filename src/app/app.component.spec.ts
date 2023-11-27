fd0e14660c5c0be380fc039f44d7a79662680489
Isabella Campos <isabellaszcamposs@gmail.com>
2023-11-27 01:32:42 -0300

    test: add tests to app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,40 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent, HeaderComponent, FooterComponent],
+      imports: [RouterTestingModule],
+    });
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  it('should render header component', () => {
+    const headerElement: HTMLElement = fixture.nativeElement.querySelector('app-header');
+    expect(headerElement).toBeDefined();
+  });
+
+  it('should render footer component', () => {
+    const footerElement: HTMLElement = fixture.nativeElement.querySelector('app-footer');
+    expect(footerElement).toBeDefined();
+  });
+
+  it('should render router outlet for main content', () => {
+    const routerOutletElement: HTMLElement = fixture.nativeElement.querySelector('router-outlet');
+    expect(routerOutletElement).toBeDefined();
   });
 });
