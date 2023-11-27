9707f5e3e8b93efd1aab8af97567947272fef834
Isabella Campos <isabellaszcamposs@gmail.com>
2023-11-27 00:43:19 -0300

    test: create header test

--- src/app/core/components/header/header.component.spec.ts
+++ src/app/core/components/header/header.component.spec.ts
@@ -1,24 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [HeaderComponent]
-    }).compileComponents();
-  });
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeaderComponent],
+    });
+
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  })
+  });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have an image with the correct source set', () => {
+    fixture.detectChanges();
+
+    const compiled: HTMLElement = fixture.nativeElement;
+    const imgElement: HTMLImageElement | null = compiled.querySelector('.header__logo');
+
+    expect(imgElement).toBeTruthy();
+    expect(imgElement!.srcset).toContain('assets/novo-logo-negativo1.svg');
   });
 });
