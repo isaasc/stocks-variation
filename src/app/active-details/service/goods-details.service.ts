2363e3bcdf85d81e6222ce7e7bfe2c00356a81f9
Isabella Campos <isabellaszcamposs@gmail.com>
2023-11-26 23:26:56 -0300

    refactor: goods-details-service

--- src/app/active-details/service/goods-details.service.ts
+++ src/app/active-details/service/goods-details.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, map, tap } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable, catchError, map, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Result } from '../models/Result.interface';
-import { Chart } from '../models/Chart.interface';
 import { ChartResponse } from '../models/ChartResponse.interface';
 
 @Injectable({
@@ -17,13 +16,32 @@ export class GoodsDetailsService {
   constructor(private http: HttpClient) { }
 
   getChart(symbol: string, period1: number, period2:number, interval: string): Observable<ChartResponse> {
-    const useYfid: boolean = true;
-    const includedPrePost: boolean = true;
-    return this.http.get<ChartResponse>(`${this.corsHeroku}/${this.urlApi}/v8/finance/chart/${symbol}?symbol=${symbol}&period1=${period1}&period2=${period2}&useYfid=${useYfid}&interval=${interval}&includedPrePost=${includedPrePost}&corsDomain=${this.corsDomain}`)
+    const params = new HttpParams()
+    .set('symbol', symbol)
+    .set('period1', period1.toString())
+    .set('period2', period2.toString())
+    .set('useYfid', 'true')
+    .set('interval', interval)
+    .set('includedPrePost', 'true')
+    .set('corsDomain', this.corsDomain)
+
+    const url: string = `${this.corsHeroku}/${this.urlApi}/v8/finance/chart/${symbol}`;
+
+    return this.http.get<ChartResponse>(url, { params }).pipe(
+      catchError(this.handleError)
+    );
   }
+
   getResult(symbol: string, period1: number, period2:number, interval: string): Observable<Result> {
     return this.getChart(symbol, period1, period2, interval)
       .pipe(
-        map(chart => chart.chart.result[0]));
+        map(chart => chart.chart.result[0]),
+        catchError(this.handleError)
+      );
+  }
+
+  handleError(error: any): Observable<never> {
+    console.error('An error occurred:', error);
+    return throwError(() => 'Something went wrong; Please try again later.');
   }
 }
