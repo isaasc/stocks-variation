4fdd6ca9f9179243e019e638e11b1604b39b0213
Isabella Campos <isabellaszcamposs@gmail.com>
2023-11-27 00:37:10 -0300

    refactor: timestamp utilitity now using date-fns lib

--- src/app/core/utilities/timestamp.utility.ts
+++ src/app/core/utilities/timestamp.utility.ts
@@ -1,46 +1,45 @@
 import { Injectable } from '@angular/core';
-import { NumberUtility } from './number.utility';
+import { format, subDays, getDay } from 'date-fns';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TimestampUtility {
 
-  constructor(private numberUtility: NumberUtility) {
+  constructor() {}
 
-  }
   calculateBusinessDaysAgo(startingDate: Date, businessDaysToSubtract: number): Date {
-    const startingDayOfWeek: number = startingDate.getDay();
     const saturday: number = 6;
     const sunday: number = 0;
 
-    if (startingDayOfWeek === sunday) {
-      startingDate.setDate(startingDate.getDate() - 2);
-    } else if (startingDayOfWeek === saturday) {
-      startingDate.setDate(startingDate.getDate() - 1);
+    if (getDay(startingDate) === sunday) {
+      startingDate = subDays(startingDate, 2);
+    } else if (getDay(startingDate) === saturday) {
+      startingDate = subDays(startingDate, 1);
     }
 
-    const currentDate = new Date(startingDate.getTime());
+    let currentDate = new Date(startingDate.getTime());
     let businessDaysCount: number = 0;
 
     while (businessDaysCount < businessDaysToSubtract) {
-      currentDate.setDate(currentDate.getDate() - 1);
-      if (currentDate.getDay() !== saturday && currentDate.getDay() !== sunday) {
+      currentDate = subDays(currentDate, 1);
+      if (getDay(currentDate) !== saturday && getDay(currentDate) !== sunday) {
         businessDaysCount++;
       }
     }
-
     return currentDate;
   }
 
-  formatTimestampsForDisplay(timestamps: string[]): string[] {
-    return timestamps.map(timestamp => this.formatSingleTimestampForDisplay(Number(timestamp)));
+  formatTimestampsForDisplay(timestamps: string[]): string[] | undefined {
+    if(!timestamps) return;
+
+    return timestamps.map(timestamp => this.formatSingleTimestampForDisplay(timestamp));
   }
 
-  formatSingleTimestampForDisplay(timestamp: number): string {
-    const date = new Date(timestamp * 1000);
-    const day = this.numberUtility.formatNumber(date.getDate(), 2);
-    const month = this.numberUtility.formatNumber((date.getMonth() + 1), 2);
-    return `${day}/${month}`;
+  formatSingleTimestampForDisplay(timestamp: string): string {
+    if(!timestamp) return '';
+
+    const date = new Date(Number(timestamp) * 1000);
+    return format(date, 'dd/MM');
   }
 }
